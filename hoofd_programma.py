5a244fffb7617aaa5bcb3871e1dbf7ebee1f28e5
Stefaandeblock <stefaan.deblock.sd@gmail.com>
2024-06-18 08:45:34 +0200

    Delete hoofd_programma.py

--- hoofd_programma.py
+++
@@ -1,8 +0,0 @@
-from helper import onderstreept
-
-uitvoer = onderstreept("aanbieding")
-uitvoer.append("Aardbeienijs, emmertje van 5 liter: 5 euro")
-uitvoer.append("Slagroom, spuitbus van 1 liter: 2 euro")
-print()
-for i in uitvoer:
-    print(i)
