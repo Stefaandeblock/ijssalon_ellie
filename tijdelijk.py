e49aa2ec3c25808c9e59f7fb79e9597b3cfe31f7
Stefaandeblock <stefaan.deblock.sd@gmail.com>
2024-06-18 09:32:11 +0200

    Hoofdstuk10

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,19 +1,18 @@
 from helper import decoreer
-
 def print_aanbieding():
     prijzen = {
-        "aardbei" : 3,
-        "vanille" : 4,
-        "chocolade" : 5
-        }
-    
-    aanbieding = (prijzen["aardbei"]*0.8)
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5
+    }
 
-    reclame_tekst = (f"vandaag in de aanbieding: aardbei, 1 liter - slechts € {aanbieding}")
+    aanbieding = prijzen["aardbei"]*0.8
 
-    reclame_tekst2 = (reclame_tekst[:59])
+    reclame_tekst = (f"Vandaag in de aanbieding: aardbei-ijs, 1liter - slechts € {aanbieding}")
 
-    reclame_tekst3 = (reclame_tekst2.upper())
+    reclame_tekst2 = reclame_tekst[:62]
+
+    reclame_tekst3 = reclame_tekst2.upper()
 
     reclame_tekst4 = (reclame_tekst3.split())
 
@@ -25,5 +24,3 @@ def print_aanbieding():
 
 decoreer("aanbieding")
 print_aanbieding()
-
-
